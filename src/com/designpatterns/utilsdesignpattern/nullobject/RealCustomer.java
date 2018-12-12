2df0a1bd4d6ff09406b5db5fed2584b4f19a79fd
Vasil <vasilsokolov0@gmail.com>
2018-12-12 11:12:09 +0200

    Add mockito

--- src/com/designpatterns/utilsdesignpattern/nullobject/RealCustomer.java
+++ src/com/designpatterns/utilsdesignpattern/nullobject/RealCustomer.java
@@ -2,6 +2,12 @@ package com.designpatterns.utilsdesignpattern.nullobject;
 
 public class RealCustomer extends AbstractCustomer {
 	
+	private String name;
+	
+	public RealCustomer(String name) {
+		this.name = name;
+	}
+
 	@Override
 	public boolean isNil() {
 		return true;
