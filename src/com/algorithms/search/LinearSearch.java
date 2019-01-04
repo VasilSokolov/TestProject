9f54736c7b284adaabc82360282050e08602c561
Vasil <vasilsokolov0@gmail.com>
2019-01-04 12:06:15 +0200

    Minor fix

--- src/com/algorithms/search/LinearSearch.java
+++ src/com/algorithms/search/LinearSearch.java
@@ -1,11 +1,11 @@
-package com.algorithms;
+package com.algorithms.search;
 
 public class LinearSearch {
     
     public static void main(String[] args){
          
         int[] arr1 = {23,45,21,55,234,1,34,90};
-        int searchKey = 34;
+        int searchKey = 0;
         System.out.println("Key "+ searchKey + " found at index: " + linerSearch(arr1, searchKey));
         int[] arr2 = {123,445,421,595,2134,41,304,190};
         searchKey = 421;
@@ -20,6 +20,7 @@ public class LinearSearch {
                 return i;
             }
         }
+        
         return -1;
     }
 }
